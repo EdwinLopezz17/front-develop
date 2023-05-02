3c79149cd072103624a049649bcc9a369c066b66
Leydi Yaquelin Mamani Aro <89089568+Leydi-Aro@users.noreply.github.com>
2023-05-01 20:53:51 -0500

    Agregando componente addproperty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,37 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AddpropertyComponent } from './components/addproperty/addproperty.component';
+
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatStepperModule} from "@angular/material/stepper";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSelectModule} from "@angular/material/select";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AddpropertyComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSelectModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/addproperty/addproperty.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+
+@Component({
+  selector: 'app-addproperty',
+  templateUrl: './addproperty.component.html',
+  styleUrls: ['./addproperty.component.css'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {showError: true},
+    },
+  ],
+})
+export class AddpropertyComponent {
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+}
