3ba5617629caa7fcae6191b03ddaab60cbc8105e
Julio Asillo Mendoza <u20201b482@upc.edu.pe>
2023-05-03 01:28:49 -0500

    adding components of add-advertisement.

---
+++ src/app/components/add-advertisement/add-advertisement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-advertisement',
+  templateUrl: './add-advertisement.component.html',
+  styleUrls: ['./add-advertisement.component.css']
+})
+export class AddAdvertisementComponent {
+
+}
