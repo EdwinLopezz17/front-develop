7bb74ead658e9c1c2b5140a4fbbd34aa5a72aae6
Julio Asillo Mendoza <u20201b482@upc.edu.pe>
2023-05-03 12:55:35 -0500

    Create user-lessor-profile.component.ts
    adding component of profile lessor.

---
+++ src/app/components/user-lessor-profile/user-lessor-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-lessor-profile',
+  templateUrl: './user-lessor-profile.component.html',
+  styleUrls: ['./user-lessor-profile.component.css']
+})
+export class UserLessorProfileComponent {
+
+}
